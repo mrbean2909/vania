f6eeac8ccfd22b878793ff4ceeac0892fbe024f7
Venia <tixonova.ulka@gmail.com>
2023-07-20 12:30:45 +0300

    первый коммит

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Автосервис "Твой"</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <div class="page1">
+    <h1 class="zagolovok">
+      <font size="20" color="#333" face="arial black">Автосервис "Твой"</font>
+    </h1>
+    <div class="container">
+      <div class="tvoy">
+        <font size="6" color="white" face="comic sans ms">Шумит? Стучит? Скрипит?</font>
+        <h1 class="what">Проверьте ходовую за 0 ₽</h1>
+        <div>
+          <button href="#" class="button1">кнопка</button>
+        </div>
+      </div>
+      <div class="picture">
+        <img class="car" src="img/car.png">
+      </div>
+    </div>
+  </div>
+  <div class="page2">
+    <div class="page2_tittle">
+      <h1>комплексная проверка автомобиля</h1>
+    </div>
+    <div class="div">
+      <div class="page2_img_div">
+        <p class="img2">
+          <img class="page2_img" src="img/2.jpg">
+        </p>
+      </div>
+      <div class="page2_text_div">
+        <h1 class="page2_text">
+          Диагностика ходовой части
+        </h1>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
