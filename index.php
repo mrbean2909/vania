5554be67166770a7e5595bcb30d3d1e851a43d22
Venia <tixonova.ulka@gmail.com>
2023-08-04 11:27:26 +0300

    все

--- index.php
+++ index.php
@@ -43,6 +43,43 @@
       </div>
       </div>
   </div>
+  <div class="page3">
+    <div class="form_page3">    
+      <form class="form">
+          <label for="dog-names">Выбор города:</label><br>
+          <select class="gorod" name="dog-names" id="dog-names">
+              <option value="rigatoni">Вологда</option>
+            <option value="dave">Череповец</option>
+          </select>
+          <br>
+          <label for="fname">Время:</label><br>
+        <label><input class="time" type="time"></label><br>
+        <label for="fname">Дата:</label><br>
+        <label><input class="date" type="date" id="datepicker"></label><br>
+        
+        <label for="dog-names">Выбор марки:</label><br>
+        <select class="marka" name="cars" id="cars">
+        <option value="car">Лада</option>
+        <option value="car">шкода</option>
+        <option value="car">рено</option>
+        <option value="car">гелик</option>
+        <option value="car">датсун</option>
+        <option value="car">тайота</option>
+        <option value="car">шурале</option>
+        <option value="car">форт</option>
+      </select>
+      <br>
+        <label for="fname">Имя:</label><br>
+        <input class="name" type="text" id="fname" name="fname" placeholder="введите имя"><br>
+        <label for="lname">Фамилия:</label><br>
+        <input class="fam" type="text" id="lname" name="lname" placeholder="введите фамилию"><br>
+        <label for="fname">Телефон:</label><br>
+        <input class="tel" type="text" id="fname" name="fname" placeholder="введите телефон"><br>
+        <label for="lname">Почта:</label><br>
+        <input class="em" type="email" id="lname" name="lname" placeholder="введите почту"><br>
+    </form>
+  </div>
+  </div>
 </body>
 
 </html>
