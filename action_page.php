e6a90a8a0d657c186c409335298710b47c55e293
Venia <tixonova.ulka@gmail.com>
2023-08-10 12:49:14 +0300

    ya programmir

--- action_page.php
+++ action_page.php
@@ -1,8 +1,36 @@
-город + 
-дата +
-время +
-марка +
-модель +
-имя пользователя +
-телефон 
-почта 
+Здравствуйте, <?php echo htmlspecialchars($_POST['name']); ?><?php echo htmlspecialchars($_POST['fam']); ?>.
+Вы записались на <?php echo($_POST['date']); ?> число.
+на <?php echo htmlspecialchars($_POST['time']); ?>.
+
+
+
+<?php
+$name = $_POST['name'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$mark = $_POST['cars'];
+// $model = $_POST['model'];
+
+$servername = "localhost";
+  
+// In my case, user name will be root
+$username = "root";
+  
+// Password is empty
+$password = "";
+$bd = "autosrevis";
+// Creating a connection
+$conn = new mysqli($servername, 
+            $username, $password, $bd);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failure: " 
+        . $conn->connect_error);
+}else{
+    echo"успех";
+}
+
+$sql = mysqli_query($conn, "INSERT INTO `zapis` (`name`, `datetime`, `tel`, `mark`, `email`) VALUES ('{$name}', '2008-10-23 10:37:22', '{$tel}', '{$mark}', '{$email}')");
--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 </head>
 
 <body>
+  <div class="www">
+    
   <div class="page1">
     <h1 class="zagolovok">
       <font size="20" color="#333" face="arial black">Автосервис "Твой"</font>
@@ -44,21 +46,21 @@
       </div>
   </div>
   <div class="page3">
-    <div class="form_page3">    
-      <form class="form">
+    <div class="form_page3">
+      <form class="form" action="action_page.php" method="post">
           <label for="dog-names">Выбор города:</label><br>
-          <select class="gorod" name="dog-names" id="dog-names">
+          <select class=" field gorod" name="cite" id="city">
               <option value="rigatoni">Вологда</option>
             <option value="dave">Череповец</option>
           </select>
           <br>
+          <label><input class="field time" type="time" name="time"></label><br> 
           <label for="fname">Время:</label><br>
-        <label><input class="time" type="time"></label><br>
         <label for="fname">Дата:</label><br>
-        <label><input class="date" type="date" id="datepicker"></label><br>
+        <label><input class="field date" type="date" id="datepicker" name="date"></label><br>
         
         <label for="dog-names">Выбор марки:</label><br>
-        <select class="marka" name="cars" id="cars">
+        <select class="field marka" name="cars" id="cars">
         <option value="car">Лада</option>
         <option value="car">шкода</option>
         <option value="car">рено</option>
@@ -69,16 +71,25 @@
         <option value="car">форт</option>
       </select>
       <br>
+        <label for="fname">Марка:</label><br>
+        <input class="field name" type="text" id="mark" name="mark" placeholder="выберете модель вашего автомобиля"><br>
         <label for="fname">Имя:</label><br>
-        <input class="name" type="text" id="fname" name="fname" placeholder="введите имя"><br>
+        <input class="field name" type="text" id="name" name="name" placeholder="введите имя"><br>
         <label for="lname">Фамилия:</label><br>
-        <input class="fam" type="text" id="lname" name="lname" placeholder="введите фамилию"><br>
+        <input class="field fam" type="text" id="fam" name="fam" placeholder="введите фамилию"><br>
         <label for="fname">Телефон:</label><br>
-        <input class="tel" type="text" id="fname" name="fname" placeholder="введите телефон"><br>
+        <input class="field tel" type="text" id="tel" name="tel" placeholder="введите телефон"><br>
         <label for="lname">Почта:</label><br>
-        <input class="em" type="email" id="lname" name="lname" placeholder="введите почту"><br>
+        <input class="field em" type="email" id="email" name="email" placeholder="введите почту"><br>
+        <button class="but" type="submit"><h1 class="but_text">записаться</h1></button>
     </form>
+    <h1 class="map"><script type="text/javascript" charset="utf-8" async 
+      src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A08a84b9bfc13c14eae691f931dff5ef1f72b7be2516e0aa08cfbc847b66fa941&amp;width=620&amp;height=480&amp;lang=ru_RU&amp;scroll=true"></script></h1>
   </div>
+  </div>
+  <footer class="footer">
+    <h1>telegram</h1>
+  </footer>
   </div>
 </body>
 
